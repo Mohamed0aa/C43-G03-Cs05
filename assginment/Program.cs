7e019d9e6a41e9a96257d48583d945c7f4b3a78c
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2024-12-26 02:53:43 +0200

    q2

--- assginment/Program.cs
+++ assginment/Program.cs
@@ -5,10 +5,15 @@
         static void Main(string[] args)
         {
             #region q1 : Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
-             //by value : it sent copy of values to function and not affect out of function
-             //by refrence: it sent actual data to function to modifiy it  and affect out of function
+            //by value : it sent copy of values to function and not affect out of function
+            //by refrence: it sent actual data to function to modifiy it  and affect out of function
             #endregion
 
+            #region q2 : Explain the difference between passing (Reference type parameters) by value and by reference then write a suitable c# example.
+            //by value : it sent the pointer  of value which point on actual data and make paramter equale it so it affect on main value
+            //by refrence:it  share pointer and main value to function 
+            //by refrence: it sent actual data to function to modifiy it  and affect out of function
+            #endregion
         }
     }
 }
