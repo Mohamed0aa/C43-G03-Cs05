e6e7801ee2eb017f3f0f0669831ba966724c5733
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2024-12-26 02:45:22 +0200

    q1

---
+++ assginment/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace assginment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region q1 : Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
+             //by value : it sent copy of values to function and not affect out of function
+             //by refrence: it sent actual data to function to modifiy it  and affect out of function
+            #endregion
+
+        }
+    }
+}
